b022e29ba453f6a09bbde5f323a192a6a9f52fd7
Lisa Folliard <lisaf37@gmail.com>
2015-10-23 21:44:17 -0700

    validate presence of image and its description

---
+++ app/models/image.rb
@@ -0,0 +1,10 @@
+class Image < ActiveRecord::Base
+  has_attached_file :image
+
+   validates :image_file_name, :presence => true
+   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+   validates :description, :presence => true
+
+  #  belongs_to :user
+end
---
+++ spec/models/image_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Image do
+  it { should validate_presence_of :image }
+  it { should validate_presence_of :description }
+
+  # it { should belong_to :user }
+end
