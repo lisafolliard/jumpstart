39bd4db7a5e0a8b37e22bbf5981049d88fe5f9c9
Lisa Folliard <lisaf37@gmail.com>
2015-10-23 21:51:09 -0700

    add form and error partials

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,46 @@
+class ImagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @user = User.find(params[:user_id])
+    @image = @user.images.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @image = @user.images.new(image_params)
+    if @image.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  # def edit
+  #   @user = User.find(params[:user_id])
+  #   @image = Image.find(params[:id])
+  # end
+  #
+  # def update
+  #   @image = Image.find(params[:id])
+  #   if @image.update(image_params)
+  #     redirect_to user_path(@image.user)
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @user = User.find(params[:user_id])
+  #   @image = Image.find(params[:id])
+  #   @image.destroy
+  #     redirect_to user_path(@user)
+  # end
+  #
+  # private
+  #
+  # def image_params
+  #   params.require(:image).permit(:image, :description)
+  # end
+
+end
