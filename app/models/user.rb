78f7db7e0b5d56096b83eb06ff706ce0acc8d998
Lisa Folliard <lisaf37@gmail.com>
2015-10-23 21:39:01 -0700

    validate presence of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :username
+
+  has_many :images
 end
