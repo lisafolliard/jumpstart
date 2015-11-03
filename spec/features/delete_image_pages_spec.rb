5022b758c5a141446bd53ab7bca0b50f8aedfa1c
Lisa Folliard <lisaf37@gmail.com>
2015-11-02 20:57:28 -0800

    pass integration spec to delete image

---
+++ spec/features/delete_image_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "delete an image" do
+  it "delete an image" do
+    login_user
+    click_link "Cat"
+    click_link "Add Image"
+    fill_in "Description", with: 'flower'
+    page.attach_file("Image", 'spec/fixtures/images/flowers.jpg')
+    click_on 'Save'
+    click_on 'Delete Image'
+    expect(page).to_not have_content "flower"
+  end
+
+  def login_user
+    visit root_path
+    click_link "Sign up"
+    fill_in "Email", with: "cat@cat.com"
+    fill_in "Username", with: "Cat"
+    fill_in "Password", with: "88888888"
+    fill_in "Password confirmation", with: "88888888"
+    click_on "Sign Up"
+  end
+end
