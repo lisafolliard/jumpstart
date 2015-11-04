8e263bfac7a4ed8b53e223f9908acb30eaf67219
Lisa Folliard <lisaf37@gmail.com>
2015-11-04 15:30:09 -0800

    validate critic latitude

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -2,6 +2,6 @@ class Critic < ActiveRecord::Base
    validates :name, :presence => true
    validates :description, :presence => true
    validates :address, :presence => true
-  #  validates :latitude, :presence => true
+   validates :latitude, :presence => true
   #  validates :longitude, :presence => true
 end
