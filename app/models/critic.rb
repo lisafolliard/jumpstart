4e88434b39a0a47057efd8dfb31cc132b7a0bc5c
Lisa Folliard <lisaf37@gmail.com>
2015-11-04 15:29:13 -0800

    validate critic description

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -1,6 +1,6 @@
 class Critic < ActiveRecord::Base
    validates :name, :presence => true
-  #  validates :description, :presence => true
+   validates :description, :presence => true
   #  validates :address, :presence => true
   #  validates :latitude, :presence => true
   #  validates :longitude, :presence => true
--- spec/models/critic_spec.rb
+++ spec/models/critic_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Critic do
   it { should validate_presence_of :name }
-  # it { should validate_presence_of :description }
+  it { should validate_presence_of :description }
   # it { should validate_presence_of :address }
   # it { should validate_presence_of :latitude }
   # it { should validate_presence_of :longitude }
