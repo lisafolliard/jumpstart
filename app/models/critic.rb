4daf90a124b2fddbd42db81745a94875b80f07f2
Lisa Folliard <lisaf37@gmail.com>
2015-11-04 15:30:38 -0800

    validate critic longitude

--- app/models/critic.rb
+++ app/models/critic.rb
@@ -3,5 +3,5 @@ class Critic < ActiveRecord::Base
    validates :description, :presence => true
    validates :address, :presence => true
    validates :latitude, :presence => true
-  #  validates :longitude, :presence => true
+   validates :longitude, :presence => true
 end
--- spec/models/critic_spec.rb
+++ spec/models/critic_spec.rb
@@ -5,5 +5,5 @@ describe Critic do
   it { should validate_presence_of :description }
   it { should validate_presence_of :address }
   it { should validate_presence_of :latitude }
-  # it { should validate_presence_of :longitude }
+  it { should validate_presence_of :longitude }
 end
