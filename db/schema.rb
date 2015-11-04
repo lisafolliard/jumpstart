12e87ee025fb3a6f9fa34e3aedfbf5b1f8a1ebfa
Lisa Folliard <lisaf37@gmail.com>
2015-11-04 11:01:45 -0800

    add about column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031233904) do
+ActiveRecord::Schema.define(version: 20151104184753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20151031233904) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
+    t.string   "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
