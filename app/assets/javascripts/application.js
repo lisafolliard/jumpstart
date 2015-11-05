b2a912177df83ca7d50ce39d984ad0f67efb7f78
Lisa Folliard <lisaf37@gmail.com>
2015-11-04 16:36:56 -0800

    begin google map api implementation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
 //= require turbolinks
 //= require_tree .
--- app/controllers/critics_controller.rb
+++ app/controllers/critics_controller.rb
@@ -1,6 +1,10 @@
 class CriticsController < ApplicationController
   def index
     @critics = Critic.all
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
   end
 
   def new
